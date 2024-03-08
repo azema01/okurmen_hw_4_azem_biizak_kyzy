74faf8bdb6379873f623c8e69da0f08d53ca0f5c
biizakkyzyazem <azemeshimova@gmail.com>
2024-03-08 18:12:09 +0600

    js 4

---
+++ index.js
@@ -0,0 +1,58 @@
+const Number = 5
+switch (Number) {
+  case 0:
+    console.log("Sunday");
+    break;
+  case 1:
+    console.log("Monday");
+    break;
+  case 2:
+    console.log("Tuesday");
+    break;
+  case 3:
+    console.log("Wednesday");
+    break;
+  case 4:
+    console.log("Thursday");
+    break;
+  case 5:
+    console.log("Friday");
+    break;
+  case 6:
+    console.log("Saturday");
+    default:
+      console.log('day');
+
+}
+
+// const number = +prompt('Сан жазыныз (0-дон, 1-ке, 2-ге, 3-ке):', '');
+
+// switch (number) {
+//   case 0:
+//     alert('Силер 0 санын жаздынар');
+//     break;
+//   case 1:
+//     alert('Силер 1 санын жаздынар');
+//     break;
+//   case 2:
+//     alert('Силер 2 санын жаздынар');
+//     break;
+//   case 3:
+//     alert('Силер 3 санын жаздынар');
+//     break;
+//   default:
+//     alert('сиз сан жазбадыныз');
+// }
+
+// const browser = prompt('Браузердин атын жазгыла', '');
+
+// if (browser === 'Edge') {
+//   alert("Бул Edge браузер!");
+// } else if (browser === 'Chrome' || browser === 'Firefox' || browser === 'Safari' || browser === 'Opera') {
+//   alert('Бул Chrome же Firefox же Safari же Opera');
+// } else {
+//   alert('Бул сенин барузерин)');
+// }
+
+
+
